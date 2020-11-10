960ec7dc4eae6cef47db09cade997ec9cd9b18c2
Raul Melo <melo.raulf@gmail.com>
2020-11-10 08:23:49 +0100

    add explanation on first page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,46 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div style={{ maxWidth: 760, margin: "0 auto" }}>
+      <h1>Next Link + Image Bug</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <p>
+        This repository reproduces a bug where we navigate from a page to other
+        with `next/link` but the image (`next/image`) does not change the
+        source.
+      </p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <section>
+        <h2>Steps to reproduce</h2>
+        <ol>
+          <li>
+            Access this repository code sandbox:{" "}
+            <a href="https://codesandbox.io/s/github/raulfdm/next-link-image-bug">
+              https://codesandbox.io/s/github/raulfdm/next-link-image-bug
+            </a>
+          </li>
+          <li>
+            Navigate to route <a href="/blog/test1">/blog/test1</a>. There
+            you'll find a top level nav with page 1 and page 2
+          </li>
+          <li>
+            Then, click in Page 2 link.
+            <strong>
+              Note that the image just change the size but not the content
+            </strong>
+          </li>
+          <li>
+            Refresh the page with F5.{" "}
+            <strong>Note that the image will now be different</strong>
+          </li>
+          <li>
+            Now click in the Page 1.{" "}
+            <strong>Note now the image will remain</strong>
+          </li>
+          <li>Refresh the page with F5 and see the first image</li>
+        </ol>
+      </section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <video src="/report.mp4" width={760} controls></video>
     </div>
-  )
+  );
 }
