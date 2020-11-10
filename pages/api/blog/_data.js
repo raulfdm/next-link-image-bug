3fd4edaa278daf38a1f281cfffe289fedad26447
Raul Melo <melo.raulf@gmail.com>
2020-11-10 08:11:30 +0100

    enhance api

--- pages/api/blog/_data.js
+++ pages/api/blog/_data.js
@@ -2,11 +2,19 @@ export const data = {
   test1: {
     slug: "test1",
     title: "Hello. Welcome to this page (Test 1)",
-    featuredImage: "/test1.jpg",
+    featuredImage: {
+      src: "/images/test1.jpg",
+      width: 1080,
+      height: 620,
+    },
   },
   test2: {
     slug: "test2",
     title: "Hello. Welcome to this page (Test 2)",
-    featuredImage: "/test2.jpg",
+    featuredImage: {
+      src: "/images/test2.jpg",
+      width: 800,
+      height: 800,
+    },
   },
 };
